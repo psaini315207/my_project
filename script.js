93e6f67b98266eaf6f2f9474a722af94c7337945
PRINCE <psaini24993@gmail.com>
2026-05-06 16:04:00 +0530

    Refactor script.js to add contact form and cursor effect

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
- function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: "smooth"
-  });
+ /* Contact Form */
+function sendMessage(e) {
+  e.preventDefault();
+  alert("Your request has been sent 🚀 I will contact you soon!");
 }
 
-function showMessage() {
-  alert("Thanks for visiting my portfolio 🚀");
-}
+/* Mouse Cursor Effect */
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+document.body.appendChild(cursor);
 
-/* Typing Effect */
-const text = ["DevOps Learner 🚀", "Future Engineer 💻", "Tech Enthusiast 🔥"];
-let i = 0;
-let j = 0;
-let currentText = "";
-let isDeleting = false;
-
-function type() {
-  if (i < text.length) {
-    if (!isDeleting && j <= text[i].length) {
-      currentText = text[i].substring(0, j++);
-    } else if (isDeleting && j >= 0) {
-      currentText = text[i].substring(0, j--);
-    }
-
-    document.getElementById("typing").innerHTML = currentText;
-
-    if (j == text[i].length) isDeleting = true;
-    if (j == 0) {
-      isDeleting = false;
-      i = (i + 1) % text.length;
-    }
-  }
-  setTimeout(type, 100);
-}
-
-type();
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
