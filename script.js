c16855980d4a4ffc991a4bf37bf3df4d1c746d4f
PRINCE <psaini24993@gmail.com>
2026-05-06 16:21:55 +0530

    Refactor script.js to enhance user interactions
    Removed mouse cursor effect and added smooth scrolling and typing effect.

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
- /* Mouse Cursor Effect */
-const cursor = document.createElement("div");
-cursor.classList.add("cursor");
-document.body.appendChild(cursor);
+ function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
 
-document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
-});
+function sendMessage(e) {
+  e.preventDefault();
+  alert("Thanks! I will contact you soon 🚀");
+}
+
+/* Typing effect */
+const text = ["DevOps Learner 🚀", "Freelancer 💻", "Future Engineer 🔥"];
+let i = 0, j = 0, current = "", del = false;
+
+function type() {
+  if (!del && j <= text[i].length) {
+    current = text[i].substring(0, j++);
+  } else if (del && j >= 0) {
+    current = text[i].substring(0, j--);
+  }
+
+  document.getElementById("typing").innerHTML = current;
+
+  if (j == text[i].length) del = true;
+  if (j == 0) {
+    del = false;
+    i = (i + 1) % text.length;
+  }
+
+  setTimeout(type, 100);
+}
+
+type();
