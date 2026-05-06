ed8a34721a65c37225c59e01d7b3fb9645f739e1
PRINCE <psaini24993@gmail.com>
2026-05-06 15:59:08 +0530

    Update greeting message and add typing effect

--- script.js
+++ script.js
@@ -5,5 +5,33 @@
 }
 
 function showMessage() {
-  alert("Hello Prince! 👋 Keep Learning DevOps 🚀");
+  alert("Thanks for visiting my portfolio 🚀");
 }
+
+/* Typing Effect */
+const text = ["DevOps Learner 🚀", "Future Engineer 💻", "Tech Enthusiast 🔥"];
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
+
+function type() {
+  if (i < text.length) {
+    if (!isDeleting && j <= text[i].length) {
+      currentText = text[i].substring(0, j++);
+    } else if (isDeleting && j >= 0) {
+      currentText = text[i].substring(0, j--);
+    }
+
+    document.getElementById("typing").innerHTML = currentText;
+
+    if (j == text[i].length) isDeleting = true;
+    if (j == 0) {
+      isDeleting = false;
+      i = (i + 1) % text.length;
+    }
+  }
+  setTimeout(type, 100);
+}
+
+type();
